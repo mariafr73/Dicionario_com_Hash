9056d2f0f8f76a48798f83cf4d4187c3365d03c7
AliceSantoi <alicelira2016@gmail.com>
2025-07-10 22:55:39 -0300

    comentários
    pendências

--- main.c
+++ main.c
@@ -3,7 +3,11 @@
 #include <string.h>
 #include <ctype.h>
 #include "../include/dict.h"
+/*o que falta: adicionar verificações: não aceitar números e símbolos (e outras aí..rs :p),
+estrutura CRUD, funcao para liberar dicionario, função para remover palavra E significado (tente aproveitar a que ja tem)
+*/
 
+/*seria legal adicionar(opcional): salvar as listas em um arquivo.txt*/
 
 int main()
 {
@@ -12,21 +16,18 @@ int main()
     inicializaLista(&lista);
     inicializaDict(&dicionario);
 
-
     char nome[] = "Alice";
     char descricao[] = "vai quebrar o codigo";
 
     char nome1[] = "Gabi";
     char descricao1[] = "vai arrumar";
 
-    
-
     adicionaNaTabela(&dicionario, nome, descricao);
     adicionaNaTabela(&dicionario, nome1, descricao1);
 
     imprimeDict(&dicionario);
 
-    //criar funcao para liberar dicionario
+    // criar funcao para liberar dicionario
 
     return 0;
 }
